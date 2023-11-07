42bad3569da5c2c0c05fda941b47b33457f0d032
tabula_rasa <rasar@hotmail.com>
2023-11-06 18:09:33 -0800

    Initial commit

---
+++ DuckDuckGoSearchApp.Tests/STATestMethodAttribute.cs
@@ -0,0 +1,14 @@
+﻿ 
+    [AttributeUsage(AttributeTargets.Method)]
+    public class STATestMethodAttribute : TestMethodAttribute
+    {
+        public override TestResult[] Execute(ITestMethod testMethod)
+        {
+            TestResult[] result = null;
+            var thread = new Thread(() => result = base.Execute(testMethod));
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            thread.Join();
+            return result;
+        }
+    }
---
+++ DuckDuckGoSearchApp.Tests/Views_MainWindowTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DuckDuckGoSearchApp.Tests
+{
+
+    [TestClass] 
+    public class Views_MainWindowTests
+    {
+        [STATestMethod]
+        public void SearchCounter_isFiveOnLoad()
+        {
+            //Arrange
+            var viewMainWindow = new MainWindow();
+            Assert.IsTrue(viewMainWindow.SearchCount == 5, "SearchCount should start at 5" );
+        }
+    }
+}
